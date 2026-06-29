2ac95ef39c700b3b785ce687e2f7098c0be3a86e
mbagherzad1369 <magherzad@gmail.com>
2026-06-29 16:32:19 +0330

    add tfaenabled to Transfers.tsx

--- src/components/transfers/Transfers.tsx
+++ src/components/transfers/Transfers.tsx
@@ -1,7 +1,9 @@
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import {Tab, TabGroup, TabList, TabPanel, TabPanels} from "@headlessui/react";
 import Templates from "~/components/transfers/templates";
 import CryptoWithdrawal from "~/components/transfers/crypto-withdrawal";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import localStorageService from "../../service/LocalstorageService";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -10,11 +12,20 @@ function classNames(...classes: string[]) {
 export default function Transfers() {
   const tabs = ["Transfer", "Whitelist Addresses"];
   const router = useRouter();
+  const [userDetails, setUserDetails] = useState<any>({
+    tfaEnabled: false,
+  });
 
+  const {tfaEnabled} = userDetails;
 
-   const handleNavigate = (path: string) => {
-     void router.push(path);
-   }
+  useEffect(() => {
+    const data = localStorageService.decodeAuthBody();
+    setUserDetails(data);
+  }, []);
+
+  const handleNavigate = (path: string) => {
+    void router.push(path);
+  }
 
   return (
     <div className="w-full mx-auto ">
@@ -24,7 +35,7 @@ export default function Transfers() {
             {tabs.map((tab) => (
               <Tab
                 key={tab}
-                className={({ selected }) =>
+                className={({selected}) =>
                   classNames(
                     "flex-1 px-8 py-3 text-sm font-bold rounded-lg transition-all duration-300 outline-none",
                     selected
@@ -40,23 +51,32 @@ export default function Transfers() {
         </div>
 
         {/* 2FA Alert */}
-        <div className="mb-0 flex items-center justify-between bg-[#FFF8F9] border border-[#FFD0DA] rounded-md px-4 py-2">
-          <div className="flex items-center gap-3">
-            <div className="flex items-center justify-center w-8 h-8 rounded-full border-2 border-[#FF3D71] text-[#FF3D71]">
-              <span className="font-bold">!</span>
+       {!tfaEnabled && (
+          <div className="mb-0 flex items-center justify-between bg-[#FFF8F9] border border-[#FFD0DA] rounded-md px-4 py-2">
+            <div className="flex items-center gap-3">
+              <div className="flex items-center justify-center w-8 h-8 rounded-full border-2 border-[#FF3D71] text-[#FF3D71]">
+                <span className="font-bold">!</span>
+              </div>
+              <p className="text-[#1A1C1E] font-medium text-sm">
+                Please enable two factor authentication to secure your account.
+              </p>
             </div>
-            <p className="text-[#1A1C1E] font-medium text-sm">Please enable two factor authentication to secure your account.</p>
+
+            <button
+              onClick={() => handleNavigate("/app/profile")}
+              className="text-[#4775F2] text-sm whitespace-nowrap font-bold hover:underline"
+            >
+              Enable Now
+            </button>
           </div>
-          <button onClick={  () => handleNavigate("/app/profile")}
-             className="text-[#4775F2] text-sm whitespace-nowrap font-bold hover:underline">Enable Now</button>
-        </div>
+        )}
 
         <TabPanels>
           <TabPanel className="focus:outline-none">
-            <CryptoWithdrawal />
+            <CryptoWithdrawal/>
           </TabPanel>
           <TabPanel className="focus:outline-none">
-            <Templates />
+            <Templates/>
           </TabPanel>
         </TabPanels>
       </TabGroup>
